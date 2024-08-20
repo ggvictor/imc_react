25eec2334ffd26a9afa07acf196933363598ab1a
ggvictor <victorrodriguessilva2@gmail.com>
2024-08-20 16:18:05 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Imc_react/imc_calc",
+  base: "/imc_react",
 })
